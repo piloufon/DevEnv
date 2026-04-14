423642d1263e9067c0fb1fd41cbf743807b2b1cc
piloufon <piloufonsansd@gmail.com>
2026-04-14 16:45:53 +0200

    Implement AES encryption and decryption functions

---
+++ cl/src/crypto/AES.cpp
@@ -0,0 +1,323 @@
+#include "crypto/AES.h"
+#include <memory>
+
+constexpr std::array<uint8_t, 256> sbox = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5,
+    0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0,
+    0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc,
+    0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a,
+    0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0,
+    0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b,
+    0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85,
+    0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5,
+    0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17,
+    0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88,
+    0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c,
+    0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9,
+    0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6,
+    0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e,
+    0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94,
+    0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68,
+    0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+};
+constexpr std::array<uint8_t, 256> sboxinv = {
+    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38,
+    0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+    0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87,
+    0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+    0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d,
+    0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+    0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2,
+    0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+    0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16,
+    0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+    0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda,
+    0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+    0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a,
+    0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+    0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02,
+    0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+    0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea,
+    0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+    0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85,
+    0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+    0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89,
+    0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+    0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20,
+    0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+    0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31,
+    0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+    0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d,
+    0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+    0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0,
+    0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26,
+    0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+};
+
+#define FE(x)  (((x) << 1) ^ ((((x)>>7) & 1) * 0x1b)) // same as rj_xtime
+#define FD(x)  (((x) >> 1) ^ (((x) & 1) ? 0x8d : 0))
+
+constexpr uint8_t xtime_forward(uint8_t x) {
+    return (x << 1) ^ (((x >> 7) & 1) * 0x1b);
+}
+inline unsigned char rj_xtime(unsigned char x) {
+    return (x & 0x80) ? ((x << 1) ^ 0x1b) : (x << 1);
+}
+void sub_bytes(std::span<uint8_t, 16> block) {
+    for (uint8_t i = 0; i < 16; i++) {
+        block[i] = sbox[block[i]];
+    }
+}
+void sub_bytes_inv(std::span<uint8_t, 16> block) {
+    for (uint8_t i = 0; i < 16; i++) {
+        block[i] = sboxinv[block[i]];
+    }
+}
+void shift_rows(std::span<uint8_t, 16> block) {
+    unsigned char i, j, k, l;
+
+    i = block[1];
+    block[1] = block[5];
+    block[5] = block[9];
+    block[9] = block[13];
+    block[13] = i;
+
+    j = block[10];
+    block[10] = block[2];
+    block[2] = j;
+
+    k = block[3];
+    block[3] = block[15];
+    block[15] = block[11];
+    block[11] = block[7];
+    block[7] = k;
+
+    l = block[14];
+    block[14] = block[6];
+    block[6] = l;
+}
+void shift_rows_inv(std::span<uint8_t, 16> block) {
+    unsigned char i, j, k, l;
+
+    i = block[1];
+    block[1] = block[13];
+    block[13] = block[9];
+    block[9] = block[5];
+    block[5] = i;
+
+    j = block[2];
+    block[2] = block[10];
+    block[10] = j;
+
+    k = block[3];
+    block[3] = block[7];
+    block[7] = block[11];
+    block[11] = block[15];
+    block[15] = k;
+
+    l = block[6];
+    block[6] = block[14];
+    block[14] = l;
+}
+void mix_columns(std::span<uint8_t, 16> block) {
+    unsigned char i, a, b, c, d, e;
+
+    for (i = 0; i < 16; i += 4) {
+        a = block[i];
+        b = block[i + 1];
+        c = block[i + 2];
+        d = block[i + 3];
+
+        e = a ^ b ^ c ^ d;
+
+        block[i] ^= e ^ rj_xtime(a ^ b);
+        block[i + 1] ^= e ^ rj_xtime(b ^ c);
+        block[i + 2] ^= e ^ rj_xtime(c ^ d);
+        block[i + 3] ^= e ^ rj_xtime(d ^ a);
+    }
+}
+void mix_columns_inv(std::span<uint8_t, 16> block) {
+    unsigned char i, a, b, c, d, e, x, y, z;
+
+    for (i = 0; i < 16; i += 4) {
+        a = block[i];
+        b = block[i + 1];
+        c = block[i + 2];
+        d = block[i + 3];
+
+        e = a ^ b ^ c ^ d;
+        z = rj_xtime(e);
+        x = e ^ rj_xtime(rj_xtime(z ^ a ^ c));  y = e ^ rj_xtime(rj_xtime(z ^ b ^ d));
+
+        block[i] ^= x ^ rj_xtime(a ^ b);
+        block[i + 1] ^= y ^ rj_xtime(b ^ c);
+        block[i + 2] ^= x ^ rj_xtime(c ^ d);
+        block[i + 3] ^= y ^ rj_xtime(d ^ a);
+    }
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+inline void AES<N>::m_encrypt(std::span<uint8_t, 16> block) {
+    add_round_key(block, 0);
+
+    for (size_t round = 1; round < NR; ++round) {
+        sub_bytes(block);
+        shift_rows(block);
+        mix_columns(block);
+        add_round_key(block, round);
+    }
+
+    sub_bytes(block);
+    shift_rows(block);
+    add_round_key(block, NR);
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+void AES<N>::m_decrypt(std::span<uint8_t, 16> block) {
+    add_round_key(block, NR);
+
+    for (size_t round = NR - 1; round >= 1; --round) {
+        shift_rows_inv(block);
+        sub_bytes_inv(block);
+        add_round_key(block, round);
+        mix_columns_inv(block);
+    }
+
+    shift_rows_inv(block);
+    sub_bytes_inv(block);
+    add_round_key(block, 0);
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+void AES<N>::key_expansion(const Key& key) {
+    auto sub_word = [](uint32_t w) -> uint32_t { // Required for template
+        return (uint32_t(sbox[(w >> 24) & 0xFF]) << 24)
+            | (uint32_t(sbox[(w >> 16) & 0xFF]) << 16)
+            | (uint32_t(sbox[(w >> 8) & 0xFF]) << 8)
+            | (uint32_t(sbox[(w) & 0xFF]));
+        };
+
+    auto rot_word = [](uint32_t w) -> uint32_t { // Required for template
+        return (w << 8) | (w >> 24);
+        };
+
+    uint32_t W[EXP_KEY_WORDS];
+
+    for (size_t i = 0; i < NK; ++i) {
+        W[i] = (uint32_t(key[4 * i]) << 24)
+            | (uint32_t(key[4 * i + 1]) << 16)
+            | (uint32_t(key[4 * i + 2]) << 8)
+            | (uint32_t(key[4 * i + 3]));
+    }
+
+    uint8_t rcon = 0x01;
+
+    for (size_t i = NK; i < EXP_KEY_WORDS; ++i) {
+        uint32_t temp = W[i - 1];
+
+        if (i % NK == 0) {
+            temp = sub_word(rot_word(temp)) ^ (uint32_t(rcon) << 24);
+            rcon = xtime_forward(rcon);
+        }
+        // AES-256 only
+        else if constexpr (NK == 8) {
+            if (i % NK == 4)
+                temp = sub_word(temp);
+        }
+
+        W[i] = W[i - NK] ^ temp;
+    }
+
+    for (size_t i = 0; i < EXP_KEY_WORDS; ++i) {
+        m_rkey[4 * i] = (W[i] >> 24) & 0xFF;
+        m_rkey[4 * i + 1] = (W[i] >> 16) & 0xFF;
+        m_rkey[4 * i + 2] = (W[i] >> 8) & 0xFF;
+        m_rkey[4 * i + 3] = (W[i]) & 0xFF;
+    }
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+void AES<N>::add_round_key(std::span<uint8_t, 16> block, size_t round) {
+    size_t offset = round * 16;
+    for (size_t i = 0; i < 16; ++i)
+        block[i] ^= m_rkey[offset + i];
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+void AES<N>::encrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out) {
+    size_t numBlocks = in.size() / 16;
+    size_t remainder = in.size() % 16;
+
+    out.resize(numBlocks * 16 + (remainder ? 16 : 0));
+
+    for (size_t i = 0; i < numBlocks; ++i) {
+        std::array<uint8_t, 16> block;
+        std::memcpy(block.data(), in.data() + i * 16, 16);
+        m_encrypt(block);
+        std::memcpy(out.data() + i * 16, block.data(), 16);
+    }
+
+    if (remainder) {
+        std::array<uint8_t, 16> block = { 0 };
+        uint8_t padValue = static_cast<uint8_t>(16 - remainder);
+        std::memcpy(block.data(), in.data() + numBlocks * 16, remainder);
+        std::memset(block.data() + remainder, padValue, 16 - remainder);
+        m_encrypt(block);
+        std::memcpy(out.data() + numBlocks * 16, block.data(), 16);
+    }
+}
+
+template<size_t N>
+    requires(N == 128 || N == 192 || N == 256)
+bool AES<N>::decrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out) {
+    if (in.size() == 0 || in.size() % 16 != 0)
+        return false;
+
+    size_t numBlocks = in.size() / 16;
+    out.resize(in.size());
+
+    for (size_t i = 0; i < numBlocks; ++i) {
+        std::array<uint8_t, 16> block;
+        std::memcpy(block.data(), in.data() + i * 16, 16);
+        m_decrypt(block);
+        std::memcpy(out.data() + i * 16, block.data(), 16);
+    }
+
+
+    uint8_t padValue = out.back();
+    if (padValue == 0 || padValue > 16)
+        return false;
+
+    for (size_t i = out.size() - padValue; i < out.size(); ++i) {
+        if (out[i] != padValue)
+            return false;
+    }
+
+    out.resize(out.size() - padValue);
+    return true;
+}
+
+template class AES<128>;
+template class AES<192>;
+template class AES<256>;
