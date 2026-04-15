2f74e7ccb21cbb96d640a36b5ba63435e5c29c21
piloufon <piloufonsansd@gmail.com>
2026-04-15 13:15:11 +0200

    Change default counterStart to 1 in CTR class
    Updated default counterStart value in encrypt and decrypt methods from 2 to 1.

--- cl/include/crypto/CTR.h
+++ cl/include/crypto/CTR.h
@@ -20,8 +20,8 @@ public:
 
     CTR(const Key& key) : m_CA(key) {}
 
-    void encrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out, std::span<const uint8_t, 12> nonce, uint32_t counterStart = 2);
-    void decrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out, std::span<const uint8_t, 12> nonce, uint32_t counterStart = 2);
+    void encrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out, std::span<const uint8_t, 12> nonce, uint32_t counterStart = 1);
+    void decrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out, std::span<const uint8_t, 12> nonce, uint32_t counterStart = 1);
 
 private:
     CA m_CA;
