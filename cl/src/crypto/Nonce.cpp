c4fecefdc859c221b651a269cf8cd1ce4c270e06
piloufon <piloufonsansd@gmail.com>
2026-04-15 07:07:06 +0200

    Refactor Nonce class with new structure and methods

--- cl/src/crypto/Nonce.cpp
+++ cl/src/crypto/Nonce.cpp
@@ -1,10 +1,26 @@
-#include "Nonce.h"
+#pragma once
+#include <span>
+#include <atomic>
+#include <mutex>
+#include "../../include/utils/SafeArray.h"
 
 template <size_t N>
-	requires (N == 12 || N == 24)
-void Nonce<N>::GetNonce(std::span<uint8_t, N> in, size_t allocation) {
-	return;
-}
+	requires (N == 12 || N == 24 /* XChacha20 only */)
+class Nonce {
+public:
+	Nonce();
 
-template class Nonce<12>;
-template class Nonce<24>;
+	// Don't go higher than a UINT32_MAX for 12 bytes nonces, the uint64_t is just for warning
+	bool GetNonce(std::span<uint8_t, N> out, uint64_t allocation);
+private:
+	static constexpr size_t RANDOM_BYTES = N * 2 / 3;
+	static constexpr size_t COUNTER_BYTES = N / 3;
+	using counter_t = std::conditional_t<N == 12, uint32_t, uint64_t>;
+
+	SafeArray<RANDOM_BYTES> m_random_part = {};
+	std::atomic<counter_t> m_incremental_part = 0;
+
+	std::mutex m_mtx;
+};
+
+
