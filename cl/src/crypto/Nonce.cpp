b290d33005c85f826b8e7ee414ba3ee4e8d1734a
piloufon <piloufonsansd@gmail.com>
2026-04-15 07:07:52 +0200

    It was Nonce.h not Nonce.cpp

--- cl/src/crypto/Nonce.cpp
+++ cl/src/crypto/Nonce.cpp
@@ -1,26 +1,43 @@
-#pragma once
-#include <span>
-#include <atomic>
-#include <mutex>
-#include "../../include/utils/SafeArray.h"
+#include "Nonce.h"
+#include <memory>
+#include "../../include/crypto/CryptoRandom.h"
 
 template <size_t N>
-	requires (N == 12 || N == 24 /* XChacha20 only */)
-class Nonce {
-public:
-	Nonce();
+	requires (N == 12 || N == 24)
+Nonce<N>::Nonce() {
+	CryptoRandom::fill(m_random_part);
+}
 
-	// Don't go higher than a UINT32_MAX for 12 bytes nonces, the uint64_t is just for warning
-	bool GetNonce(std::span<uint8_t, N> out, uint64_t allocation);
-private:
-	static constexpr size_t RANDOM_BYTES = N * 2 / 3;
-	static constexpr size_t COUNTER_BYTES = N / 3;
-	using counter_t = std::conditional_t<N == 12, uint32_t, uint64_t>;
+template <size_t N>
+	requires (N == 12 || N == 24)
+bool Nonce<N>::GetNonce(std::span<uint8_t, N> out, uint64_t allocation) {
+	if constexpr (N == 12) {
+		if (allocation >= UINT32_MAX) { // TODO : Log warning
+			return false;
+		}
+	}
 
-	SafeArray<RANDOM_BYTES> m_random_part = {};
-	std::atomic<counter_t> m_incremental_part = 0;
+	{
+		std::lock_guard<std::mutex> lock(m_mtx);
 
-	std::mutex m_mtx;
-};
+		if ((uint64_t)(allocation + m_incremental_part) >= UINT32_MAX - 1) {
+			CryptoRandom::fill(m_random_part);
+			m_incremental_part = allocation;
+		}
+		else {
+			m_incremental_part += allocation;
+		}
+	}
+
+	std::memcpy(out.data(), m_random_part.data(), RANDOM_BYTES);
+
+	for (uint8_t i = 0; i < COUNTER_BYTES; i++) {
+		out[RANDOM_BYTES + i] = (m_incremental_part >> ((COUNTER_BYTES - 1 - i) * 8)) & 0xFF;
+	}
+
+	return true;
+}
 
 
+template class Nonce<12>;
+template class Nonce<24>;
