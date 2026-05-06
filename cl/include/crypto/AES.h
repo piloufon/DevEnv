eb1c1f0bed78bb0ee2ee99b8e6796f4ad328848d
piloufon <piloufonsansd@gmail.com>
2026-05-06 02:45:11 +0200

    Refactor AES class to use KEY_SIZE constant and add cipher_vaes declaration

--- cl/include/crypto/AES.h
+++ cl/include/crypto/AES.h
@@ -5,13 +5,13 @@
 template <size_t N>
     requires(N == 128 || N == 192 || N == 256)
 class AES {
-    static constexpr size_t KEY_SIZE = N / 8;
+    static constexpr size_t KEY_SIZE = N / 8; // 16 - 24 - 32
     static constexpr size_t NR = (N / 32) + 6;
     static constexpr size_t NK = N / 32;
     static constexpr size_t EXP_KEY_WORDS = (NR + 1) * 4;
     static constexpr size_t EXP_KEY_BYTES = EXP_KEY_WORDS * 4;
 public:
-    using Key = SafeArray<N / 8>;
+    using Key = SafeArray<KEY_SIZE>;
 
     AES(const Key& key);
     ~AES() {
@@ -23,24 +23,30 @@ public:
     bool decrypt(std::span<const uint8_t> in, std::vector<uint8_t>& out);
 
     template<size_t BLOCKS = 1>
+        requires (BLOCKS == 1 || BLOCKS == 2 || BLOCKS == 4 || BLOCKS == 8)
     void encrypt_block(std::span<uint8_t, BLOCKS * 16> block);
+
     template<size_t BLOCKS = 1>
+        requires (BLOCKS == 1 || BLOCKS == 2 || BLOCKS == 4 || BLOCKS == 8)
     void decrypt_block(std::span<uint8_t, BLOCKS * 16> block);
 private:
     SafeArray<EXP_KEY_BYTES> m_rkey;
     SafeArray<EXP_KEY_BYTES> m_rkey_inv;
 
     template<size_t BLOCKS>
-    void cipher_aesni(std::span<uint8_t, BLOCKS * 16> block);
+        requires (BLOCKS == 2 || BLOCKS == 4 || BLOCKS == 8 || BLOCKS == 16)
+    void cipher_vaes256(const uint8_t* in, uint8_t* out);    
     template<size_t BLOCKS>
-    void cipher_aesni_inv(std::span<uint8_t, BLOCKS * 16> block);
+        requires (BLOCKS == 4 || BLOCKS == 8 || BLOCKS == 16)
+    void cipher_vaes512(const uint8_t* in, uint8_t* out);
 
+    template<size_t BLOCKS>
+        requires (BLOCKS == 1 || BLOCKS == 2 || BLOCKS == 4 || BLOCKS == 8)
+    void cipher_aesni(const uint8_t* in, uint8_t* out);
+    template<size_t BLOCKS>
+        requires (BLOCKS == 1 || BLOCKS == 2 || BLOCKS == 4 || BLOCKS == 8)
+    void cipher_aesni_inv(const uint8_t* in, uint8_t* out);
 
-    void add_round_key(std::span<uint8_t, 16> block, size_t round);
-
-    void cipher(std::span<uint8_t, 16> block);
+    void cipher(std::span<uint8_t, 16> block); // Don't really care 
     void cipher_inv(std::span<uint8_t, 16> block);
-
-    void key_expansion(const Key& key);
-    void key_expansion_inv();
 };
