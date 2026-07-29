883cebaa4f0cc22e17b9ac07f19eb582c3becd6d
piloufon <piloufonsansd@gmail.com>
2026-07-29 02:17:31 +0200

    Delete cl/include/crypto/CipherConcepts.h

--- cl/include/crypto/CipherConcepts.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <concepts>
-
-template<typename CA>
-concept CipherAlgorithm = requires(CA ca, std::span<uint8_t, 16> block) {
-    typename CA::Key;
-    { ca.encrypt_block(block) } -> std::same_as<void>; // Return type after declaration are so weird
-    { ca.decrypt_block(block) } -> std::same_as<void>; //
-};
